4b1d23c8e671015449e5da401c54dc725174348e
Andrey Eroshkin <andreroshkin@gmail.com>
2019-12-16 18:26:40 +0300

    wp theme

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    url : 'http://localhost/rosinter/',
+    themeDir : '/wp-content/themes/rosinter-theme/',
+    fullPath : 'http://localhost/rosinter/wp-content/themes/rosinter-theme/',
+    ip  : "192.168.50.4"
+}
---
+++ page.php
@@ -0,0 +1,8 @@
+<?php
+
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+
+$templates = ['page-' . $post->post_name . '.twig', 'page.twig'];
+Timber::render( $templates, $context );
