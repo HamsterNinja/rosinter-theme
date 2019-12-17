cdd8356d0d0f01c8b382ccd5284d8bef8204d7f4
Andrey Eroshkin <andreroshkin@gmail.com>
2019-12-17 14:28:57 +0300

    404 page

---
+++ archive-teachers.php
@@ -0,0 +1,7 @@
+<?php
+
+$context = Timber::get_context();
+
+
+$templates = ['archive-teachers.twig'];
+Timber::render( $templates, $context );
---
+++ include/theme-settings.php
@@ -0,0 +1,32 @@
+<?php
+add_action( 'init', 'create_my_post_types' );
+function create_my_post_types() {
+    register_post_type(
+        'courses',
+         array(
+            'labels' => array( 'name' => __( 'Курсы' ),
+            'singular_name' => __( 'Курс' ) ),
+            'supports'      => array( 'title', 'editor', 'thumbnail','excerpt'),
+            'has_archive' => true,
+            'show_in_rest' => true,
+            'public' => true, ) );      
+    register_post_type(
+        'reviews',
+         array(
+            'labels' => array( 'name' => __( 'Отзывы' ),
+            'singular_name' => __( 'Отзыв' ) ),
+            'supports'      => array( 'title', 'editor', 'thumbnail','excerpt'),
+            'has_archive' => true,
+            'show_in_rest' => true,
+            'public' => true, ) );  
+    
+    register_post_type(
+        'teachers',
+         array(
+            'labels' => array( 'name' => __( 'Учителя' ),
+            'singular_name' => __( 'Учитель' ) ),
+            'supports'      => array( 'title','editor', 'thumbnail','excerpt'),
+            'has_archive' => true,
+            'show_in_rest' => true,
+            'public' => true, ) );              
+}
