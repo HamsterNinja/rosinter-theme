d1d8d188d8e60c48f6df97950bfc8cc3e90090a9
Andrey Eroshkin <andreroshkin@gmail.com>
2019-12-20 16:53:29 +0300

    new review

--- include/theme-settings.php
+++ include/theme-settings.php
@@ -36,3 +36,32 @@ function create_my_post_types() {
             'public' => true, ) );              
 }
 
+include_once(get_template_directory() . '/include/generate_featured_image.php');
+
+if( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) &&  $_POST['action'] == "new_reviews") {
+        
+    if (isset ($_POST['title'])) {
+        $title =  $_POST['title'];
+    } 
+    if (isset ($_POST['description'])) {
+        $description = $_POST['description'];
+    } 
+    require_once(ABSPATH . 'wp-admin/includes/image.php');
+    require_once(ABSPATH . 'wp-admin/includes/file.php');
+    require_once(ABSPATH . 'wp-admin/includes/media.php');
+    $uploadedfile = $_FILES['file'];
+    $upload_overrides = array( 'test_form' => false );
+    $movefile = wp_handle_upload( $uploadedfile, $upload_overrides );
+    $movefile = $movefile['url'];
+    
+    
+    $new_post = array(
+        'post_title'    => $title,
+        'post_content'  => $description,
+        'post_status'   => 'draft',           // Choose: publish, preview, future, draft, etc.
+        'post_type' => 'reviews' //'post',page' or use a custom post type if you want to
+    );
+    $pid = wp_insert_post($new_post); 
+    
+    Generate_Featured_Image( $movefile, $pid );
+}
