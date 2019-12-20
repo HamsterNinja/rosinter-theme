e7f8f596b96567c1dd7ed703cf65b3e03dcacd74
Andrey Eroshkin <andreroshkin@gmail.com>
2019-12-20 15:52:25 +0300

    account

---
+++ author.php
@@ -0,0 +1,6 @@
+<?php
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+$context['current_user'] = new Timber\User();
+Timber::render( array( 'account.twig' ), $context );
